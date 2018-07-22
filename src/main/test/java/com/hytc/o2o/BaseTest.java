09c4556c516afe271ea2bfd9ac3ade6d564b0b29
gcl <gcl142536>
2018-07-22 11:23:34 +0800

    项目环境搭建v0.01

---
+++ src/main/test/java/com/hytc/o2o/BaseTest.java
@@ -0,0 +1,16 @@
+package com.hytc.o2o;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * 用来初始化Spring和Junit整合，启动时候家在Spring容器
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml","classpath:spring/spring-web.xml"})
+@WebAppConfiguration("src/main/resources")
+public class BaseTest {
+
+}
---
+++ src/main/test/java/com/hytc/o2o/service/AreaServiceTest.java
@@ -0,0 +1,24 @@
+package com.hytc.o2o.service;
+
+import com.hytc.o2o.BaseTest;
+
+import com.hytc.o2o.entity.Area;
+import com.hytc.o2o.service.impl.AreaService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AreaServiceTest extends BaseTest {
+
+    @Autowired
+    private AreaService areaService;
+
+    @Test
+    public void testGetAreaList(){
+       List<Area> areaList = areaService.getAreaList();
+       assertEquals(2,areaList.size());
+    }
+}
