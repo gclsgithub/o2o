f05341fb31fe331f7ebe8dc744d1bcf364ab5ed8
gcl <gcl142536>
2018-10-22 22:29:09 +0800

    10。22前台Index界面向各个界面跳转

--- src/main/java/com/hytc/o2o/controller/front/FrontJumpMapperController.java
+++ src/main/java/com/hytc/o2o/controller/front/FrontJumpMapperController.java
@@ -5,47 +5,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("jump")
+@RequestMapping("/frontend")
 public class FrontJumpMapperController {
 
     /**
      * 主页
+     *
      * @return
      */
-    @RequestMapping(path = "index",method = RequestMethod.GET)
-    public String jumpToIndex(){
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String jumpToIndex() {
         return "/frontend/index";
     }
 
     /**
      * 消费记录
      */
-    @RequestMapping(value = "myrecord",method = RequestMethod.GET)
-    public String jumpMySaleRecod(){
-        return "frontend/myrecord";
+    @RequestMapping(value = "/myrecord", method = RequestMethod.GET)
+    public String jumpMySaleRecod() {
+        return "/frontend/myrecord";
     }
 
     /**
      * 个人积分
      */
-    @RequestMapping(value = "mypoint",method = RequestMethod.GET)
-    public String jumpPersonalCode(){
-        return "frontend/mypoint";
+    @RequestMapping(value = "/mypoint", method = RequestMethod.GET)
+    public String jumpPersonalCode() {
+        return "/frontend/mypoint";
     }
 
     /**
      * 积分兑换记录
      */
-    @RequestMapping(value = "pointrecord",method = RequestMethod.GET)
-    public String jumpPointRecord(){
-        return "frontend/pointrecord";
+    @RequestMapping(value = "/pointrecord", method = RequestMethod.GET)
+    public String jumpPointRecord() {
+        return "/frontend/pointrecord";
     }
 
     /**
      * 积分兑换记录
      */
-    @RequestMapping(value = "shoplist",method = RequestMethod.GET)
-    public String jumpShopList(){
-        return "frontend/shoplist";
+    @RequestMapping(value = "/shoplist", method = RequestMethod.GET)
+    public String jumpShopList() {
+        return "/frontend/shoplist";
     }
 }
